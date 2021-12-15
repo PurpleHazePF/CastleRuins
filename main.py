89fa6b4a2b7e40c397f62743c6ec73854501ed20
Fjodor <fjodor.doronin@yandex.ru>
2021-12-15 16:14:22 +0300

    player values update

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from player import player
+from player import *
 pygame.init()
 size = width, height = 800, 600
 screen = pygame.display.set_mode(size)
@@ -7,16 +7,29 @@ clock = pygame.time.Clock()
 screen.fill("gray")
 pygame.display.set_caption("Руины замка")
 running = True
-pl = player()
-pygame.draw.circle(screen, "red", (pl.x, pl.y), 10)
+pygame.draw.circle(screen, "red", (x, y), 10)
 logic = False
+clock = pygame.time.Clock()
 while running:
     for event in pygame.event.get():
         keys = pygame.key.get_pressed()
         if event.type == pygame.QUIT:
             running = False
         screen.fill("gray")
-        pl.movement()
-        pygame.draw.circle(screen, "red", (pl.x, pl.y), 10)
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            x += 0
+            y += -speed
+        if keys[pygame.K_s]:
+            x += 0
+            y += speed
+        if keys[pygame.K_a]:
+            x += -speed
+            y += 0
+        if keys[pygame.K_d]:
+            x += speed
+            y += 0
+        pygame.draw.circle(screen, "red", (x, y), 10)
+    clock.tick(fps)
     pygame.display.flip()
 pygame.quit()
--- player.py
+++ player.py
@@ -1,22 +1,3 @@
-import pygame
-
-
-class player:
-    def __init__(self):
-        self.x = 300
-        self.y = 400
-
-    def movement(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_w]:
-            self.x += 0
-            self.y += -5
-        if keys[pygame.K_s]:
-            self.x += 0
-            self.y += 5
-        if keys[pygame.K_a]:
-            self.x += -5
-            self.y += 0
-        if keys[pygame.K_d]:
-            self.x += 5
-            self.y += 0
+x, y = 400, 300
+speed = 7
+fps = 30
