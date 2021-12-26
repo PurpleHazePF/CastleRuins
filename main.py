d4346c8d1ca209511de0b54277c44a3821eb0d5c
Slash <ew.kolesnikov1969@yandex.ru>
2021-12-27 01:07:13 +0500

    Сделал масштабирование карты Больше привязал к задаваемымы переменным некоторые свойства карты перенес в файл map Сделал движение более плавным

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import math
 import os
 import pygame
 import sys
+
 from map import *
 from player import *
 
@@ -15,18 +16,19 @@ def l_image(name):
     return image
 
 
-def check(coords, size1, x2, y2, size2):
+def check(coords, sizex, sizey, x2, y2, size2):
     for a, b in coords:  # проверка на пересечение прямоугольника игрока и прямоугольника препятствия
-        if ((a <= x2 <= a + size1 and b <= y2 <= b + size1) or
-                (a <= x2 + size2 <= a + size1 and b <= y2 + size2 <= b + size1) or
-                (a <= x2 + size2 <= a + size1 and b <= y2 <= b + size1) or
-                (a <= x2 <= a + size1 and b <= y2 + size2 <= b + size1)):
+        a, b = a + 1, b + 1  # тк отсчет от нуля,то добавляю потерянное,иначе будет расстояниемежду препятствием игероем
+        if ((a <= x2 <= a + sizex and b <= y2 <= b + sizey) or
+                (a <= x2 + size2 <= a + sizex and b <= y2 + size2 <= b + sizey) or
+                (a <= x2 + size2 <= a + sizex and b <= y2 <= b + sizey) or
+                (a <= x2 <= a + sizex and b <= y2 + size2 <= b + sizey)):
             return True
     return False
 
 
 pygame.init()
-size = width, height = 900, 650
+size = width, height  # задаются в файле map, как и другие свойства карты
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 pygame.display.set_caption("Руины замка")
@@ -42,28 +44,25 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     if pygame.key.get_pressed()[pygame.K_w]:
-        if 900 > x + math.cos(vector) * 5 > 0:
-            for _ in range(5):  # для более плавного движения, вместо 5, 5 раз по 1
+        for _ in range(speed):  # для более плавного движения, вместо 5 + проверка, 5 раз по 1 и каждый раз проверка
+            if width - SIZE + 1 > x + math.cos(vector) > 0:
                 x += math.cos(vector)
-                if check(map_cord, BLOCK_SIZE, x, y, SIZE):  # проверка на столкновение с препядствием
+                if check(map_cord, BLOCK_SIZE_X, BLOCK_SIZE_Y, x, y, SIZE):  # проверка на столкновение с препядствием
                     x -= math.cos(vector)
-        if 650 > y + math.sin(vector) * 5 > 0:
-            for _ in range(5):
+            if height - SIZE + 1 > y + math.sin(vector) > 0:
                 y += math.sin(vector)
-                if check(map_cord, BLOCK_SIZE, x, y, SIZE):
+                if check(map_cord, BLOCK_SIZE_X, BLOCK_SIZE_Y, x, y, SIZE):
                     y -= math.sin(vector)
     if pygame.key.get_pressed()[pygame.K_s]:
-        if 900 > x - math.cos(vector) * 5 > 0:
-            for _ in range(5):
+        for _ in range(speed):
+            if width - SIZE + 1 > x - math.cos(vector) > 0:
                 x -= math.cos(vector)
-                if check(map_cord, BLOCK_SIZE, x, y, SIZE):
+                if check(map_cord, BLOCK_SIZE_X, BLOCK_SIZE_Y, x, y, SIZE):
                     x += math.cos(vector)
-        if 650 > y - math.sin(vector) * 5 > 0:
-            for _ in range(5):
+            if height - SIZE + 1 > y - math.sin(vector) > 0:
                 y -= math.sin(vector)
-                if check(map_cord, BLOCK_SIZE, x, y, SIZE):
+                if check(map_cord, BLOCK_SIZE_X, BLOCK_SIZE_Y, x, y, SIZE):
                     y += math.sin(vector)
-            # проверка что персонаж на поле
     if pygame.key.get_pressed()[pygame.K_a]:
         vector -= 0.07
         if vector > 180:
@@ -84,7 +83,7 @@ while running:
                      (x + SIZE * 0.5 + math.cos(vector) * dlina_vzglyada[0],
                       y + SIZE * 0.5 + math.sin(vector) * dlina_vzglyada[1]))
     for i, j in map_cord:  # отрисовка препятствий
-        pygame.draw.rect(screen, (8, 75, 108), (i, j, BLOCK_SIZE, BLOCK_SIZE))
+        pygame.draw.rect(screen, (8, 75, 108), (i, j, BLOCK_SIZE_X, BLOCK_SIZE_Y))
     clock.tick(fps)
     pygame.display.flip()
 pygame.quit()
--- map.py
+++ map.py
@@ -6,11 +6,14 @@ text_map = [
     '.........',
     'W.......W',
     '....W....',
+    'W.......W',
     'W.......W'
 ]   # example map
 map_cord = set()
-
+width, height = 900, 650
+BLOCK_SIZE_X = (width / len(text_map[0])) // 1 + 1
+BLOCK_SIZE_Y = (height / len(text_map)) // 1 + 1
 for j, row in enumerate(text_map):
     for i, bloc in enumerate(row):
         if bloc == 'W':
-            map_cord.add((i * BLOCK_SIZE, j * BLOCK_SIZE))
+            map_cord.add((i * BLOCK_SIZE_X, j * BLOCK_SIZE_Y))
