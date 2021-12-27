27e72794a2aaa63a4d868add0d96bb713f5c9ab5
Andrei24511 <lp24511@mail.ru>
2021-12-27 22:06:43 +0500

    улучшенная версия отображения

--- main.py
+++ main.py
@@ -29,17 +29,11 @@ class Stena(pygame.sprite.Sprite):  # для отрисовки стен
 
 
 class Raycast(pygame.sprite.Sprite):
-    def __init__(self, group, a, b, r):
+    def __init__(self, group, r):
         super().__init__(group)
         self.ray = r
         self.nach_x = x + SIZE * 0.5
         self.nach_y = y + SIZE * 0.5
-        self.kon_x = a
-        self.kon_y = b
-        self.rect = pygame.Rect((self.nach_x, self.nach_y, self.kon_x, self.kon_y))
-        p_h = PROJ_COEFF / depth
-        pygame.draw.rect(screen, 'ORANGE', (ray * SCALE, height / 2 - p_h // 2, SCALE, p_h))
-        # pygame.draw.line(screen, "red", (self.nach_x, self.nach_y), (self.kon_x, self.kon_y), 2)
 
     def update(self):
         self.nach_x = x + SIZE * 0.5
@@ -47,17 +41,55 @@ class Raycast(pygame.sprite.Sprite):
         cur_angle = vector - OBZOR / 2 + DELTA_ANGLE * self.ray  # откладывем нужный угол
         sin_a = math.sin(cur_angle)
         cos_a = math.cos(cur_angle)
-        for depth in range(MAX_DEPTH):  # находим точку касания луча с ближайшей стеной
-            x1 = x + depth * cos_a
-            y1 = y + depth * sin_a
-            if (x1 // BLOCK_SIZE_X * BLOCK_SIZE_X, y1 // BLOCK_SIZE_Y * BLOCK_SIZE_Y) in map_cord:
-                depth *= math.cos(vector - cur_angle)  # убираем эффект выпуклости стен
-                self.kon_x = x1
-                self.kon_y = y1
-                p_h = PROJ_COEFF / depth  # получаем высоту проекции
-                pygame.draw.rect(screen, 'ORANGE',
-                                 (self.ray * SCALE, height // 2 - p_h // 2, SCALE, p_h))  # отображаем стену
-                break
+        x0 = self.nach_x // BLOCK_SIZE_X * BLOCK_SIZE_X
+        y0 = self.nach_y // BLOCK_SIZE_Y * BLOCK_SIZE_Y
+
+        if cos_a >= 0:  # находим блюжайшую вертикаль в зависимости от знака угла
+            m = x0 + BLOCK_SIZE_X
+            dm = 1  # устанавливаем шаг между вертикалями
+        else:
+            m = x0
+            dm = -1
+
+        for i in range(0, width, int(BLOCK_SIZE_X)):  # проходим по вертикалям пока не найдем пересечение со стеной
+            if cos_a != 0:
+                depth_v = (m - self.nach_x) / cos_a
+            else:
+                depth_v = m
+            k = self.nach_y + depth_v * sin_a  # находим координаты пересечения с вертикалью
+            if ((m + dm) // BLOCK_SIZE_X * BLOCK_SIZE_X, k // BLOCK_SIZE_Y * BLOCK_SIZE_Y) in map_cord:
+                break  # проверка на пересечение со стеной
+            m += dm * BLOCK_SIZE_X  # переходим к седующей вертикали
+
+        if sin_a >= 0:  # находим блюжайшую горизонталь в зависимости от знака угла
+            k = y0 + BLOCK_SIZE_Y
+            dk = 1  # устанавливаем шаг между горизонталями
+        else:
+            k = y0
+            dk = -1
+
+        for j in range(0, height, int(BLOCK_SIZE_Y)):  # проходим по горизонталям пока не найдем пересечение со стеной
+            if sin_a != 0:
+                depth_h = (k - self.nach_y) / sin_a
+            else:
+                depth_h = k
+            m = self.nach_x + depth_h * cos_a  # находим координаты пересечения с горизонталью
+            if (m // BLOCK_SIZE_X * BLOCK_SIZE_X, (k + dk) // BLOCK_SIZE_Y * BLOCK_SIZE_Y) in map_cord:
+                break  # проверка на пересечение со стеной
+            k += dk * BLOCK_SIZE_Y  # переходим к следующей горизонтали
+
+        if depth_v < depth_h:  # выбираем из горизонтали и вертикали ближайшую к нам
+            depth = depth_v
+        else:
+            depth = depth_h
+
+        depth *= math.cos(vector - cur_angle)  # убираем эффект выпуклости стен
+        if depth != 0:
+            p_h = PROJ_COEFF / depth  # получаем высоту проекции
+        else:
+            p_h = 0
+        pygame.draw.rect(screen, 'ORANGE',
+                         (self.ray * SCALE, height // 2 - p_h // 2, SCALE, p_h))  # отображаем стену
 
 
 class Persona(pygame.sprite.Sprite):  # для перемещения и отрисовки персонажа
@@ -91,18 +123,9 @@ sprite.rect = sprite.image.get_rect()
 all_sprites.add(sprite)
 
 vector = 0
-cur_angle = vector - OBZOR / 2
-for ray in range(NUM_RAYS):  # задаем изначальные лучи
-    sin_a = math.sin(cur_angle)  # считаем необходимые константы
-    cos_a = math.cos(cur_angle)
-    for depth in range(MAX_DEPTH):
-        x1 = x + depth * cos_a  # вычисляем координаты точки
-        y1 = y + depth * sin_a
-        if (x1 // BLOCK_SIZE_X * BLOCK_SIZE_X,
-            y1 // BLOCK_SIZE_Y * BLOCK_SIZE_Y) in map_cord:  # проверяем является ли точка крайней
-            Raycast(rays, x1, y1, ray)  # создаем луч
-            break
-    cur_angle += DELTA_ANGLE
+
+for i in range(NUM_RAYS):  # создаем лучи
+    Raycast(rays, i)
 
 for i, j in map_cord:
     Stena(steny, i, j)  # инициализируем стены
